ab150679ab1720fc7bac6a9c6f3f86eaff89c6ff
junior <les_love_20@hotmail.com>
2019-08-15 17:50:03 -0500

    inicio

---
+++ vs.code/main.js
@@ -0,0 +1,52 @@
+function getFirstValue(a){
+    return a[0];
+}
+ 
+ 
+
+function addition (a,b){
+    return a+b;
+}
+
+function area(x,y){
+    return x*y/2;
+}
+
+function rango(l1,l2){
+    return(l1+l2)-1;
+}
+
+function adition(a){
+    return a+1;
+}
+
+function length(str){
+    return str.length
+    
+}
+
+function remainder(x,y){
+    return x % y;
+}
+
+function junior(num){
+    return num<=0;
+}
+
+function addUp(num){
+    let sum =0;
+    for (let i =1; i <= num; i++)
+    { 
+    sum +=i;
+
+    }
+    return sum;
+}
+
+function minMax(arr) {
+    const min = Math.min(...arr);
+    const max = Math.max(...arr);
+    
+    return [min, max];
+  }
+
